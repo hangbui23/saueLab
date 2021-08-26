da6a2599b3d834dcb61b1b8b9f5ee330f1bf03a3
hangbui23 <53377966+hangbui23@users.noreply.github.com>
2021-08-26 22:27:13 +0700

    Update BaseUIs.java

--- src/main/java/pageUIs/BaseUIs.java
+++ src/main/java/pageUIs/BaseUIs.java
@@ -1,5 +1,8 @@
 package pageUIs;
 
 public class BaseUIs {
-
+	public static final String ICON_WAIT="//input[@id='user-name']";
+	public static final String ALL_UPLOAD_BUTTON="//input[@id='user-name']";
+	public static final String UPLOAD_FILE_TYPE="//input[@file='type']";
+	public static final String LOADED_FILE_NAME = "";
 }
