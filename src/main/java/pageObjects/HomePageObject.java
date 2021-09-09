c613e5d68b565cf7ac987e70950dfc9f5056454e
hangbui23 <53377966+hangbui23@users.noreply.github.com>
2021-09-09 08:54:42 +0700

    Update HomePageObject.java

--- src/main/java/pageObjects/HomePageObject.java
+++ src/main/java/pageObjects/HomePageObject.java
@@ -1,7 +1,7 @@
 package pageObjects;
 import org.openqa.selenium.WebDriver;
-
 import commons.BasePage;
+import io.qameta.allure.Step;
 import pageUIs.HomePageUI;
 
 public class HomePageObject extends BasePage{
@@ -10,16 +10,19 @@ public class HomePageObject extends BasePage{
 		this.driver = driver;
 	}
 	
+	@Step("Input to username textbox with value: {0}")
 	public void inputValueToUserNameTextBox(String value) {
 		waitForElementVisible(driver, HomePageUI.TXT_USERNAME);
 		senKeyToElement(driver, HomePageUI.TXT_USERNAME, value);
 	}
 	
+	@Step("Input to password textbox with value: {0}")
 	public void inputValueToPasswordTextBox(String value) {
 		waitForElementVisible(driver, HomePageUI.TXT_PASSWORD);
 		senKeyToElement(driver, HomePageUI.TXT_PASSWORD, value);
 	}
 	
+	@Step("Click on Login button")
 	public ProductPageObject clickOnLogInButton() {
 		waitForElementClickable(driver, HomePageUI.BTN_LOGIN);
 		clickToElement(driver, HomePageUI.BTN_LOGIN);
