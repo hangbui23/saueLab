2c8010d2d9914b3e1d47ca2966ba46404ce06576
hangbui23 <53377966+hangbui23@users.noreply.github.com>
2021-09-09 08:56:14 +0700

    Update ProductPageObject.java

--- src/main/java/pageObjects/ProductPageObject.java
+++ src/main/java/pageObjects/ProductPageObject.java
@@ -1,6 +1,7 @@
 package pageObjects;
 import org.openqa.selenium.WebDriver;
 import commons.BasePage;
+import io.qameta.allure.Step;
 import pageUIs.ProductPageUI;
 
 public class ProductPageObject extends BasePage{
@@ -9,23 +10,28 @@ public class ProductPageObject extends BasePage{
 		this.driver = driver;
 	}
 	
+	@Step("Select Sort combobox with value: {0}")
 	public void selectSortCombobox(String value) {
 		waitForElementVisible(driver, ProductPageUI.CBB_SORT);
 		selectItemInDropDown(driver, ProductPageUI.CBB_SORT, value);
 	}
 	
+	@Step("Verify Product Name sort desc")
 	public boolean isProdductNameSortDesc() {
 		return isDataTextSortDESC(driver, ProductPageUI.LST_PRODUCT_NAME);
 	}
 	
+	@Step("Verify Product Name sort asc")
 	public boolean isProdductNameSortAsc() {
 		return isDataTextSortASC(driver, ProductPageUI.LST_PRODUCT_NAME);
 	}
 	
+	@Step("Verify Product price sort desc")
 	public boolean isProdductPriceSortDesc() {
 		return isDataFloatSortDESC(driver, ProductPageUI.LST_PRODUCT_PRICE);
 	}
 	
+	@Step("Verify Product price sort asc")
 	public boolean isProdductPriceSortAsc() {
 		return isDataFloatSortASC(driver, ProductPageUI.LST_PRODUCT_PRICE);
 	}
